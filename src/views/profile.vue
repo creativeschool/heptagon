bb90b9fe00b7aedf00460f03a2f5fb96fd51bb47
ZhangZisu <admin@zhangzisu.cn>
2019-07-20 23:55:56 +0800

    ⛏ Profile UI

---
+++ src/views/profile.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout wrap align-content-start>
+    <v-flex xs12 class="mb-2">
+      <v-card>
+        <v-card-title>基本信息</v-card-title>
+        <v-card-text>
+          <v-text-field type="email" label="邮箱"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn color="error" outlined>重置</v-btn>
+          <v-btn color="primary">提交</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 class="mb-2">
+      <v-card>
+        <v-card-title>修改密码</v-card-title>
+        <v-card-text>
+          <v-text-field type="password" label="原密码"/>
+          <v-text-field type="password" label="新密码"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn color="primary">提交</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
